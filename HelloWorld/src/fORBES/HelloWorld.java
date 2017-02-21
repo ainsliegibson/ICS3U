ad7e7706a0bb437f64424a74f5d65aa68fa7ed1a
Ainslie Forbes <324009646@C-4JNJVR1.tdsb.on.ca>
2017-02-21 11:22:02 -0500

    Hello Ms. Kemp

---
+++ HelloWorld/src/fORBES/HelloWorld.java
@@ -0,0 +1,16 @@
+package fORBES;
+/**
+ * HelloWorld.java
+ * This program prints "Hello World" to the console
+ * February 21, 2017
+ * @author Ainslie Forbes
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+
+	}
+
+}
