58d4040259498459181d14d7eeb7f1072dc0caf7
ainslieforbes <ainslieforbes@10.17.193.100>
2017-04-19 18:02:24 -0400

    Added comments

--- ICS3UP/src/forbes/Palindrome.java
+++ ICS3UP/src/forbes/Palindrome.java
@@ -1,38 +1,56 @@
 package forbes;
-
+/** Palindrome.java
+ * This program determines if a sentence is a Palindrome and returns true or false;
+ * @author ainslieforbes
+ * Apr 19
+ */
 import java.util.Scanner;
 
 public class Palindrome {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Please enter a sentence");
+		System.out.println("Please enter a sentence");//Get sentence
 		String sentence = sc.nextLine();
 		sc.close();
-		String word = noSpace(sentence);
-		System.out.println(isPalindrome(word));
+		String word = noSpace(sentence);//send to noSpace method
+		System.out.println(isPalindrome(word)); 
+		//sent to isPalindrome and print true or false to console 
 	}
-	public static boolean isPalindrome(String letters){
-		int n = letters.length() - 1;
-		for (int i = 0; i < letters.length(); i++){
-			if(letters.charAt(i) == letters.charAt(n)){
+	public static boolean isPalindrome(String letters){ 
+		//determines if the sentence is palindrome
+		int n = letters.length() - 1; 
+		//counting variable will start at the end of the sentence
+		
+		for (int i = 0; i < letters.length(); i++){ 
+			//for loop will start at the beginning of the sentence
+			// comparing the first and last letter of the sentence
+			if(letters.charAt(i) == letters.charAt(n)){ 
+				//if they are the same continue in the loop
 				n--;
 			}
 			else{
+				//if they are the different return false
 				return false;
 			}
 		}
+		//if they are all the same return true
 		return true;
 
 	}
 	public static String noSpace(String word){
+		//take a sentence and returns it in capitals and without period, space, commas, etc.
 		String space ="";
-		word = word.toUpperCase();
+		word = word.toUpperCase();//makes the sentence all upper-case
+		//counting variable to check every space in the String
 		for (int i = 0; i < word.length(); i ++){
+			// check if the spaces in String is a capital letter (not a space, period, etc.)
 			if ((((Character)(word.charAt(i))).toString().matches("[A-Z]")) == true){
-				space = space + word.charAt(i);	
+				//add each char that is a capital letter to a new String
+				space = space + word.charAt(i);		
 			}
+			//if not a capital letter it will be skipped and not added to the new String
 		}
-		return space;
+		return space;//sends the new String with only capital letters
 	}
 }
--- ICS3UP/src/forbes/RightAngleTriangle.java
+++ ICS3UP/src/forbes/RightAngleTriangle.java
@@ -1,26 +1,33 @@
 package forbes;
-
+/** RightAngleTriangle.java
+ * This program tells what are all the possible number between 1-100 that would make a perfect right triangle
+ * @author ainslieforbes
+ * Apr 19
+ */
 public class RightAngleTriangle {
 
 	public static void main(String[] args) {
-		for(int a = 1; a <= 100; a++){ 
-			for (int b = 1; b <= 100; b++){
-				int c = (int)((Math.pow(a, 2)) + (Math.pow(b, 2)));
+		for(int a = 1; a <= 100; a++){ //triangle side, starts on 1 and goes to 100
+			for (int b = 1; b <= 100; b++){// triangle side, starts on 1 and goes to 100
+				int c = (int)((Math.pow(a, 2)) + (Math.pow(b, 2))); //Pythagorean Theorem
 
-				if (isSquare(c)){
+				if (isSquare(c)){ //sends to isSquare method
 					System.out.println(a + ", " + b + ", "+ (int)(Math.sqrt(c)));
+					//prints out possibilities
 				}	
 			}
 		}
 	}
-
 	public static boolean isSquare(int n){
+		//determines if it is a perfect square
 		int srt = (int)(Math.sqrt(n));
-		double check = Math.pow(srt, 2);
-		if (n == check){
+		//gets the square root of a number and puts it into an int(cuts off any decimal places
+		double check = Math.pow(srt, 2); 
+		//square the number that was just square rooted to see if it matches the original number
+		if (n == check){//if it does match return true
 			return true;
 		}
-		else{
+		else{//if it does not match return false
 			return false;
 		}
 	}
